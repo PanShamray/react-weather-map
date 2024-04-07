d33d246590f2f877b11b8cbb630097f2dbcb9287
Vladislav Shamray <127450674+PanShamray@users.noreply.github.com>
2024-04-08 01:14:43 +0300

    add wind direction func

--- src/components/SearchWeather/SearchWeather.jsx
+++ src/components/SearchWeather/SearchWeather.jsx
@@ -1,17 +1,19 @@
 import React, { useState/* , useEffect */ } from "react";
 import { weatherApiResponse } from "../../utils/weatherApiResponse";
+ import { getWindDirection } from "../../utils/getWindDirection";
 
 function SearchWeather() {
   
   const [cityName, setCityName] = useState('');
   const [weatherData, setWeatherData] = useState([]);
+  
 
   const apiKey = "f36464ed89d663794630263c14054ea2";
   const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=ua&APPID=${apiKey}`;
   
   const handleSubmitForm = (event) => {
     event.preventDefault();
-    weatherApiResponse(apiUrl, setWeatherData);
+    weatherApiResponse(apiUrl,setWeatherData);
     setCityName('');
   }
 
@@ -49,7 +51,7 @@ function SearchWeather() {
             <p>Вологість: {weatherData.main.humidity}%</p>
             <p>Тиск: {weatherData.main.pressure}hPa</p>
             <p>Швидкість вітру: {weatherData.wind.speed} м/с</p>
-            <p>Напрям вітру: {weatherData.wind.deg} градусів</p>
+            <p>Напрям вітру: {getWindDirection(weatherData.wind.deg)}</p>
           </>
         )}
       </div>
---
+++ src/utils/getWindDirection.js
@@ -0,0 +1,22 @@
+export function getWindDirection(windDeg) {
+  let value = '';
+
+  if ((windDeg >= 331 && windDeg <= 359) || (windDeg >= 0 && windDeg <= 30)) {
+    value = "Північний";
+  } else if (windDeg >= 31 && windDeg <= 60) {
+    value = "Північно Західний";
+  } else if (windDeg >= 61 && windDeg <= 120) {
+    value = "Західний";
+  } else if (windDeg >= 121 && windDeg <= 150) {
+    value = "Південно Західний";
+  } else if (windDeg >= 151 && windDeg <= 210) {
+    value = "Південний";
+  } else if (windDeg >= 211 && windDeg <= 240) {
+    value = "Південно Східний";
+  } else if (windDeg >= 241 && windDeg <= 300) {
+    value = "Східний";
+  } else if (windDeg >= 301 && windDeg <= 330) {
+    value = "Північно Східний";
+  }
+  return value;
+  };
